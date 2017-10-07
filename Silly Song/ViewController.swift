56b1731c47aa991ddf19d274385d0a0ed9f4f8a5
Cinfwat Dogak <cdogakz@yahoo.com>
2017-10-07 02:43:06 +0100

    Wire up the UI with the ViewController
    Use the shortNameFromName and lyricsForName function to generate the silly song and display to the user

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Silly Song
 //
-//  Created by Cinfwat Probity on 07/10/2017.
+//  Created by Cinfwat Dogak on 07/10/2017.
 //  Copyright © 2017 Dogak Cinfwat. All rights reserved.
 //
 
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+    }
 }
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+// Returns lowercase version of the provided name where all the consonants before the first vowel have been removed.
+func shortNameFromName(name: String) -> String {
+    let lowercaseName = name.lowercased()
+    let vowels = CharacterSet(charactersIn: "aeiou")
+    
+    if let range = lowercaseName.rangeOfCharacter(from: vowels) {
+        return String(lowercaseName[range.lowerBound...])
+    }
+    
+    return lowercaseName
+}
+
+// Returns the silly song lyrics with all placeholder text replaced.
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameFromName(name: fullName)
+    
+    let sillyLyrics = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return sillyLyrics
+}
+
+// Silly song template.
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
 
